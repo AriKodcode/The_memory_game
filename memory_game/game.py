766c801becfb5c53c73bbb4c75970f80c1689b10
Ari Durlacher <arikodcode@gmail.com>
2025-10-28 12:47:45 +0200

    added fonction check_matrix

--- memory_game/game.py
+++ memory_game/game.py
@@ -1,25 +1,26 @@
 from random import randrange
 
-def create_matrix(x_y_size : dict):
+
+def create_matrix(x_y_size: dict):
     x = x_y_size['x']
     y = x_y_size['y']
     matrix = []
 
     if x % 2 == 0 and y % 2 == 0:
         if x_y_size['x'] and x_y_size['y'] == 2:
-            return True
+            return "win"
         else:
-           for i in range(x):
-               vector = []
-               for j in range(y):
-                   vector.append("")
-               matrix.append(vector)
+            for i in range(x):
+                vector = []
+                for j in range(y):
+                    vector.append("")
+                matrix.append(vector)
         return matrix
     else:
         return False
 
 
-def create_cards(x_y_size : dict):
+def create_cards(x_y_size: dict):
     card_list = []
     x = x_y_size["x"]
     y = x_y_size["y"]
@@ -31,7 +32,7 @@ def create_cards(x_y_size : dict):
     return card_list
 
 
-def shuffling_cards(deck : list):
+def shuffling_cards(deck: list):
     result = deck
     for i in range(1000):
         index1 = randrange(len(deck))
@@ -42,13 +43,15 @@ def shuffling_cards(deck : list):
             result[index1], result[index2] = result[index2], result[index1]
     return result
 
-def matrix_initialization_for_x(matrix : list):
+
+def matrix_initialization_for_x(matrix: list):
     for vector in matrix:
         for char in range(len(vector)):
             vector[char] = "x"
     return matrix
 
-def matrix_mixing(matrix : list, shuffling_cards2 : list):
+
+def matrix_mixing(matrix: list, shuffling_cards2: list):
     counter = 0
     for vector in matrix:
         for char in range(len(vector)):
@@ -56,8 +59,12 @@ def matrix_mixing(matrix : list, shuffling_cards2 : list):
             counter += 1
     return matrix
 
-
-
-
-
-
+def check_matrix(check):
+    if check == list:
+        return 'list'
+    elif check == "win":
+        return "win"
+    elif not check:
+        return False
+    else:
+        return False
